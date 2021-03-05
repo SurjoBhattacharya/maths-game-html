91809241f71212543e11374a6fc89ed74a165671
SurjoBhattacharya <70901652+SurjoBhattacharya@users.noreply.github.com>
2021-03-05 09:52:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,139 @@
+//sorry this code might be messy looooooooooooooooooooooooooooooooooooooool
+//connect with the html elements 
+let questionSpace = document.getElementById('question');
+let count = document.getElementById('countdown');
+let answer = document.getElementById('input');
+let score = document.getElementById('score');
+let highScore = document.getElementById('bestScore');
+highScore.innerHTML = localStorage.getItem('bestScore');
+
+let scoreNumber = 0;
+
+//this function generates random questions and calls answerCheck();
+function question() {
+  let num1;
+  let num2;
+  let sign = Math.round(Math.random()*3);
+  if(scoreNumber <= 10) {  
+  num1 = Math.round(Math.random()*5) 
+  num2 = Math.round(Math.random()*5); 
+
+
+
+ if(sign == 0) { 
+    questionSpace.innerHTML = num1 + " + " + num2;
+    answerCheck(0,num1,num2);
+  } else if(sign == 1) { 
+    questionSpace.innerHTML = num1 + " - " + num2;
+    answerCheck(1,num1,num2);
+  }  else if(sign = 2) { 
+     questionSpace.innerHTML = num1 + " * " + num2;
+     answerCheck(2,num1,num2);
+  } 
+
+   }  else  if(scoreNumber > 10 && scoreNumber <=15){ 
+     console.log("f");
+      num1 =   Math.round(Math.random()*20);
+      num2 = Math.round(Math.random()*20);  
+      
+ if(sign == 0) { 
+  questionSpace.innerHTML = num1 + " + " + num2;
+  answerCheck(0,num1,num2);
+} else if(sign == 1) { 
+  questionSpace.innerHTML = num1 + " - " + num2;
+  answerCheck(1,num1,num2);
+}  else if(sign = 2) { 
+   questionSpace.innerHTML = num1 + " * " + num2;
+   answerCheck(2,num1,num2);
+}   
+   } else if(scoreNumber > 20 && scoreNumber <= 25) { 
+    num1 =   Math.round(Math.random()*100);
+    num2 = Math.round(Math.random()*100);  
+    
+if(sign == 0) { 
+questionSpace.innerHTML = num1 + " + " + num2;
+answerCheck(0,num1,num2);
+} else if(sign == 1) { 
+questionSpace.innerHTML = num1 + " - " + num2;
+answerCheck(1,num1,num2);
+}  else if(sign = 2) { 
+ questionSpace.innerHTML = num1 + " * " + num2;
+ answerCheck(2,num1,num2);
+} 
+   } else { 
+    num1 =   Math.round(Math.random()*scoreNumber*5);
+    num2 = Math.round(Math.random()*scoreNumber*5);  
+    
+if(sign == 0) { 
+questionSpace.innerHTML = num1 + " + " + num2;
+answerCheck(0,num1,num2);
+} else if(sign == 1) { 
+questionSpace.innerHTML = num1 + " - " + num2;
+answerCheck(1,num1,num2);
+}  else if(sign = 2) { 
+ questionSpace.innerHTML = num1 + " * " + num2;
+ answerCheck(2,num1,num2);
+} 
+   }
+}
+question();
+function answerCheck(type,num1,num2) { 
+    var num = 5;
+    let interval = setInterval(function() {  
+      num--;
+      countdown.innerHTML = num;
+      if(num <= 0) { 
+
+ if(type == 0) { 
+
+     if(answer.value == num1 + num2) { 
+       console.log('correct0');
+         scoreNumber++;
+       score.innerHTML = scoreNumber;
+       clearInterval(interval);
+     } else { 
+         getHighScore();
+         document.write('your score is: ' + scoreNumber + " , get better at maths fool!");
+         clearInterval(interval);
+     }
+
+  } else if(type == 1) { 
+
+   if(answer.value == num1 - num2) { 
+    console.log("correct1");
+    scoreNumber++; 
+    score.innerHTML = scoreNumber;
+    clearInterval(interval);
+   } else { 
+       getHighScore();
+       document.write("your score is: " + scoreNumber + ", get better at maths fool!");
+       clearInterval(interval);
+   }
+
+  } else if(type == 2) { 
+   if(num1 * num2 == answer.value) { 
+      console.log("correct3");
+      scoreNumber++;
+      score.innerHTML = scoreNumber;
+      clearInterval(interval);
+   } else { 
+       getHighScore();
+       document.write("your score is: " + scoreNumber + " , get better at maths fool!");
+       clearInterval(interval);
+   }
+  }
+question();
+}
+   },1000);
+}
+
+function getHighScore() { 
+    if(parseInt(localStorage.getItem('bestScore')) < scoreNumber) { 
+        localStorage.setItem('bestScore',scoreNumber);
+    }
+}
+
+if(localStorage.getItem('injected') == 'false') { 
+  localStorage.setItem('bestScore',0);
+  localStorage.setItem('injected',true);
+  }
